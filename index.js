e7b49fd34cd9bbe614111c9e036c16a248102f29
Frank0606 <frankdanielsanchez55@gmail.com>
2024-03-11 20:03:29 -0600

    MVC con NodeJS

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express()
 dotenv.config()
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.send("Hola mundo con daemon!")
-})
+const homeRouter = require('./routes/home')
+
+app.use("/", homeRouter)
+app.use("/home", homeRouter)
+
+const errorHandler = require('./middlewares/errorhandler')
+app.use(errorHandler)
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Aplicacion de ejemplo escuchando en el puerto ${process.env.SERVER_PORT}`)
--- models/alumno.js
+++ models/alumno.js
@@ -2,8 +2,8 @@ const pool = require("../data/datacontext")
 
 const list = async function () {
     const [rows, fields] = await pool.query(
-        `SELECT alumno.id, alumno.nombre, alumno.promedio, materia.nombre AS materia_nombre FROM materia INNER JOIN alumno ON materia.id=alumno.id`
-    );
+        `SELECT alumno.id, alumno.nombre, alumno.promedio, materia.nombre AS materia_nombre 
+        FROM materia INNER JOIN alumno ON materia.id=alumno.id_materia`);
 
     return rows
 }
